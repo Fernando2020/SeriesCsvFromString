d30e163412326ebd6e48de0889832886087e0505
Fernando Nogueira <fgeraldo1111@gmail.com>
2022-08-16 21:58:58 -0300

    Atualização da query

--- src/SeriesCsvFromString.Service/Services/WriteTxtService.cs
+++ src/SeriesCsvFromString.Service/Services/WriteTxtService.cs
@@ -27,14 +27,22 @@ namespace SeriesCsvFromString.Console.Services
 
         public void WriteCommandInsertInInTxt(List<Candle> candles, string path)
         {
-            string serie_id = @"'{a2679342-932c-4502-8139-00b63989666e}'";
             var command = new StringBuilder();
+
+            command.AppendLine($@"DELETE FROM CANDLES;");
+            command.AppendLine($@"DELETE FROM SERIES;");
+            command.AppendLine($@"DELETE FROM ASSETS;");
+            command.AppendLine($@"INSERT INTO ASSETS(ID, SYMBOL, DESCRIPTION, CHANGE_DATE, CREATION_DATE)
+            VALUES(uuid_generate_v4() ,'CCM', 'COTAÇÕES DO MILHO', current_timestamp, current_timestamp);");
+            command.AppendLine($@"INSERT INTO SERIES(ID, TIMEFRAME, ASSET_ID, CHANGE_DATE, CREATION_DATE)
+            VALUES(uuid_generate_v4() , 12, (SELECT ID FROM ASSETS ORDER BY CREATION_DATE DESC LIMIT 1), current_timestamp, current_timestamp);");
+            
             foreach (var candle in candles)
             {
                 command.AppendLine($@"INSERT INTO CANDLES (ID, TIME, OPEN, HIGH, LOW, CLOSE, TICK_VOLUME, SPREAD, REAL_VOLUME, SERIE_ID, CHANGE_DATE, CREATION_DATE)
                     VALUES(uuid_generate_v4(),{candle.Time}, {candle.Open.ToString(CultureInfo.InvariantCulture)}, {candle.High.ToString(CultureInfo.InvariantCulture)}, 
                     {candle.Low.ToString(CultureInfo.InvariantCulture)}, {candle.Close.ToString(CultureInfo.InvariantCulture)}, 
-                    {candle.TickVolume}, {candle.Spread}, {candle.RealVolume}, {serie_id}, current_timestamp, current_timestamp);
+                    {candle.TickVolume}, {candle.Spread}, {candle.RealVolume}, (SELECT ID FROM SERIES ORDER BY CREATION_DATE DESC LIMIT 1), current_timestamp, current_timestamp);
                     {Environment.NewLine}");
             }
 
